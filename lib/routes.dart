bacdb69c030b13926171e1bf687add676dbedb1e
eyad-hussein <eyad.hussein@ejust.edu.eg>
2023-12-20 16:41:56 +0200

    Finish ui of sign-up screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,10 +1,12 @@
 import 'package:app/screens/login_screen.dart';
+import 'package:app/screens/signup_screen.dart';
 import 'package:get/get.dart';
 import 'package:app/screens/splash_screen.dart';
 
 class Routes {
   static String splashScreenRoute = '/splash-screen';
   static String loginScreenRoute = '/login-screen';
+  static String signupScreenRoute = '/signup-screen';
 }
 
 final getPages = [
@@ -15,5 +17,9 @@ final getPages = [
   GetPage(
     name: Routes.loginScreenRoute,
     page: () => const LoginScreen(),
+  ),
+  GetPage(
+    name: Routes.signupScreenRoute,
+    page: () => const SignUpScreen(),
   ),
 ];
